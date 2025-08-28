2b04290e62ca4ee6801642d4a3893347e851bc5b
Ignacio <ignaciocantoisado@gmail.com>
2025-08-28 16:19:45 -0400

    After button variable and state lesson

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "I am a programer!"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.orange)
+            Button("Click Me!") {
+                message = "Awsome!"
+            }
         }
         .padding()
     }
