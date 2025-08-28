32ab10d953b1a34792de325ae2c5e9d63e130897
Ignacio <ignaciocantoisado@gmail.com>
2025-08-28 15:07:45 -0400

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Ignacio Canto on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
