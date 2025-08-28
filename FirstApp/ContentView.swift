e4c8fa6b971f6562b7341cea58d01fd47a9c16cb
Ignacio <ignaciocantoisado@gmail.com>
2025-08-28 16:33:36 -0400

    Button and formating lesson

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -11,18 +11,31 @@ struct ContentView: View {
     @State private var message = "I am a programer!"
     var body: some View {
         VStack {
+            Spacer()
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundStyle(.orange)
-            Button("Click Me!") {
-                message = "Awsome!"
+                .fontWeight(.ultraLight)
+            
+            Spacer()
+            
+            HStack{
+                Button("Awsome!") {
+                    message = "Awsome!"
+                    }
+                Button("Great!") {
+                    message = "Great!"
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
+                .tint(.orange)
+            
             }
-        }
         .padding()
     }
 }
