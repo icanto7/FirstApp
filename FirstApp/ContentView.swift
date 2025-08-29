46078021d26de829ef5a97b69dd9ea2e4c3c7951
Ignacio <ignaciocantoisado@gmail.com>
2025-08-29 12:56:04 -0400

    CH 1.6 lesson

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -24,14 +24,13 @@ struct ContentView: View {
             Spacer()
             
             Button("Press Me!") {
-                if (message == "You are Awsome!") {
-                    message = "You are Great!"
-                    centerImage = "hand.thumbsup"
-                } else {
-                    message = "You are Awsome!"
-                    centerImage = "sun.max.fill"
-                }
+                let message1 = "You are Awsome!"
+                let message2 = "You are Great!"
+                let imageString1 = "hand.thumbsup"
+                let imageString2 = "sun.max.fill"
                 
+                message = (message == message1 ? message2 : message1)
+                centerImage = (centerImage == imageString1 ? imageString2 : imageString1)
             }
                 .buttonStyle(.borderedProminent)
                 .font(.title2)
